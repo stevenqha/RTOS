d8338c755970165d0647788ea0c962cba98318fd
Steven Ha <steven.ha.v2@gmail.com>
2019-07-11 18:27:31 -0400

    Intial commit with project files

---
+++ Objects/main_default.d
@@ -0,0 +1,11 @@
+.\objects\main_default.o: main_default.c
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.5.0\Device\Include\LPC17xx.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
+.\objects\main_default.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main_default.o: C:\Users\happy\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.5.0\Device\Include\system_LPC17xx.h
+.\objects\main_default.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\main_default.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
