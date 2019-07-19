2168b60b17468e2b3e049b549725f6907cfeb035
Ben Duo <ben.duo99@gmail.com>
2019-07-19 17:55:50 -0400

    add semaphore structure

--- rtos.c
+++ rtos.c
@@ -6,20 +6,51 @@
 
 
 
+//disable interrupts
+
+//enable interrupts
+
 
 //Create Mutex
 
 
 //Obtain Mutex
-
+void osAcquireMutex(void *arg){
+	
+};
 
 //Release Mutex
+void osReleaseMutex(void *arg){
+	
+};
+
+//Define Semaphore
+typedef uint32_t osSem_Id;
+
+//Initialize Semaphore
+void init_Sema(osSem_Id *s, uint32_t n)
+{
+    &s = n;
+}
 
 
-//Create Semaphore
+//Wait Semaphore
+void osSemWait(osSem_Id *s)
+{
+    //disable interrupts
+    while(*s =< 0)
+    {
+        //enable int
+        //disable int
+    }
+    (*s)--;
+    //enable int
+}
 
-
-//Obtain Semaphore
-
-
-//Release Semaphore
+//Signal Semaphore
+void osSemSignal(osSem_Id *s)
+{
+    //disable inter
+    (s*)++;
+    //enable inter
+}
